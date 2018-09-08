ad7766943e410dbe6879cce9c23e604cc1fe924c
tangciwei <tangciwei@baidu.com>
2018-09-08 23:37:41 +0800

    ok

--- format.js
+++ format.js
@@ -7,8 +7,16 @@ let promisify = util.promisify;
 let readFile = promisify(fs.readFile);
 let writeFile = promisify(fs.writeFile);
 
-async function start() {
-    let data = await readFile('./src.md.js', 'utf8');
+async function start(name) {
+    // let name = process.argv.slice(2)[0];
+    if (!name) {
+        console.log('请输入名称');
+        return;
+    }
+
+    let prename = './markdown/';
+    name = prename + name;
+    let data = await readFile(`./${name}.md`, 'utf8');
     // #号处理和**处理
     data = data.split('\n').map((item, line) => {
         if (item[0] === '#') {
@@ -19,8 +27,9 @@ async function start() {
         item = item.replace(/\*\*([^\*]+)\*\*/g, ($0, $1) => {
             return `**${$1.trim()}**`;
         });
+
         // 图片处理
-        item = item.replace(/\!\[[^\]]+\]\(([^\)]+)\)/g, ($0, $1) => {
+        item = item.replace(/\!\[[^\]]*\]\(([^\)]+)\)/g, ($0, $1) => {
 
             if ($1.slice(0, 4) !== 'http') {
                 // 去掉./
@@ -48,6 +57,7 @@ async function start() {
 
     data = data.join('\n');
 
-    await writeFile('./out.md', data);
+    await writeFile(`./${name}.md`, data);
 }
-start();
+
+module.exports = start;
